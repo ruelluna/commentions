84d6e1de815494100df2d54d697a437f224d03ce
Navneet Rai <navneetrai@gmail.com>
2025-08-09 11:50:50 -0400

    Implement opt-in notifications for user mentions in comments

--- config/commentions.php
+++ config/commentions.php
@@ -57,4 +57,28 @@ return [
         // Automatically subscribe a user when they are mentioned in a comment
         'auto_subscribe_on_mention' => true,
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Notifications (opt-in)
+    |--------------------------------------------------------------------------
+    |
+    | Configure notification delivery when a user is mentioned in a comment.
+    | Disabled by default; enable and choose the channels you want to use.
+    |
+    */
+    'notifications' => [
+        'mentions' => [
+            'enabled' => false,
+
+            'channels' => ['mail'],
+
+            'listener' => \Kirschbaum\Commentions\Listeners\SendUserMentionedNotification::class,
+            'notification' => \Kirschbaum\Commentions\Notifications\UserMentionedInComment::class,
+
+            'mail' => [
+                'subject' => 'You were mentioned in a comment',
+            ],
+        ],
+    ],
 ];
