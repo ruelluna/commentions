f1f3369970c4d6b58ff01bf123a843534b03c7ab
Silviu Dobre <silviu@codeniner.com>
2025-09-22 15:10:56 +0300

    Added translatable support for subscriptions and Romanian translations

---
+++ resources/lang/ro/comments.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'label' => 'Comentarii',
+    'no_comments_yet' => 'Niciun comentariu încă.',
+
+    'user_avatar_alt' => 'Avatar',
+
+    'commented_at' => 'Adăugat la :datetime',
+    'edited_at' => 'Editat la :datetime',
+    'edited' => 'editat',
+
+    'delete_comment_heading' => 'Șterge comentariu',
+    'delete_comment_body' => 'Sigur vrei să ștergi acest comentariu? Această acțiune nu poate fi anulată.',
+
+    'cancel' => 'Anulare',
+    'delete' => 'Șterge',
+    'save' => 'Salvare',
+    'comment' => 'Comentariu',
+    'add_reaction' => 'Adaugă reacție',
+
+
+    'notifications' => 'Notificări',
+    'unsubscribe' => 'Dezabonare',
+    'subscribe' => 'Abonare',
+    'subscribers' => 'Abonați',
+    'more' => 'mai mulţi',
+
+];
